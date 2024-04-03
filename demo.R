54c53f40bdca21ac6b0fdaf3dd60904e89779251
Rick Weber <863114+RickWeber@users.noreply.github.com>
2024-04-03 11:58:35 -0400

    Update demo code.

--- demo.R
+++ demo.R
@@ -5,45 +5,37 @@ library(rvest)
 
 url <- "https://www.imdb.com/title/tt15239678/?ref_=hm_fanfav_tt_t_1_pd_fp1_r"
 
-# first_page <- read_html(url)
-# 
-# links <- first_page %>% 
-#   html_elements("a") %>% 
-#   html_attr("href")
-# 
-# links <- links[str_detect(links, "^/title/tt")]
-# #links[str_detect(links, "tt15239678")]
-
 strip_url <- function(url){
   str_extract(url, "/title/tt[0-9]*/")
 }
-# 
-# links <- sapply(links, strip_url)
-# # strip names off vector
-# names(links) <- NULL
-# 
-# links <- unique(links)
 
-get_page <-function(link){...}
+get_page <-function(link){
+  politely(read_html)(link)
+}
 
-get_more <- function(rel_link){
+get_links <- function(rel_link){
   full_link <- paste0("https://www.imdb.com", rel_link)
-  links <- read_html(full_link) %>% 
-    html_elements("a") %>% 
-    html_attr("href")
+  page <- get_page(full_link)
+  links <- page %>% html_elements("a") %>% html_attr("href")
   links <- links[str_detect(links, "^/title/tt")]
   links <- sapply(links, strip_url)
   # strip names off vector
   names(links) <- NULL
   links <- unique(links)
   count <- length(links)
-  return(list(count=count, links=links))
-}
-
-get_title <- function(rel_link){
-  full_link <- paste0("https://www.imdb.com", rel_link)
-  read_html(full_link) %>% 
+  title <- page %>%
     html_element("title") %>% 
     html_text() %>% 
     str_remove(" - IMDb")
+  return(list(title = title,
+              link_count=count, 
+              rel_links=links))
 }
+
+Dune2 <- get_links(strip_url(url))
+
+out <- tibble(
+  title = Dune2$title,
+  link_count = Dune2$link_count,
+  links = Dune2$rel_links)
+
